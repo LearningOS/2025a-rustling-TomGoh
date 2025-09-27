26034f773413147d509adc1e2f4acf5e713be53d
wuhaoze <wuhaoze@kylinos.cn>
2025-09-27 15:01:02 +0800

    Correct clippt and algorithm exercises

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,5 +1,5 @@
 // clippy3.rs
-// 
+//
 // Here's a couple more easy Clippy fixes, so you can see its utility.
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
@@ -15,15 +15,15 @@ fn main() {
         -1, -2, -3,
         -4, -5, -6
     ];
-    println!("My array! Here it is: {:?}", my_arr);
+    println!("My array! Here it is: {my_arr:?}");
 
     let mut my_empty_vec = vec![1, 2, 3, 4, 5];
-    my_empty_vec.resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    my_empty_vec.clear();
+    println!("This Vec is empty, see? {my_empty_vec:?}");
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
     std::mem::swap(&mut value_a, &mut value_b);
-    println!("value a: {}; value b: {}", value_a, value_b);
+    println!("value a: {value_a}; value b: {value_b}");
 }
